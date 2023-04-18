5770a4d1a7de3c7603fc10d40ffe9545324debb5
gav193 <116613315+gav193@users.noreply.github.com>
2023-04-18 08:57:25 +0700

    edit fungsi kumpul()
    batch kumpul belum bisa akses jumlah jin pengumpul dan fungsi random.
    fungsi belum dibuat cara nge-save ke csv

--- JinKumpul.py
+++ JinKumpul.py
@@ -1,10 +1,36 @@
-import Gabung
-
+import csv
+import RNG as rand
+bahan = open("bahan_bangunan.csv", 'w', newline=' ')
+writer = csv.writer(bahan, delimiter=',')
+cols = ['nama', 'deskripsi', 'jumlah']
 def kumpul(x) : 
     if x == 1 : # jin pengumpul indiv
-        #
+        pasir = rand()
+        batu = rand()
+        air = rand()
+        print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
+        # isi dalam file csv bahan
     else : # batch kumpul
-        #
+        n =  # jumlah jin pengumpul
+        if n > 0 : # ada jin pengumpul
+            pasir = [0 for i in range(n)]
+            batu = [0 for i in range(n)]
+            air = [0 for i in range(n)]
+            mat = [pasir,batu,air]
+            for i in range(3) : 
+                for j in range(n) :
+                    mat[i][j] = rand()
+            sumpas = 0
+            sumbat = 0 
+            sumair = 0
+            for i in range(n) :
+                sumpas += mat[0][j]
+                sumbat += mat[1][j]
+                sumair += mat[2][j]
+            print(f"Mengerahkan {n} jin untuk mengumpulkan bahan.")
+            print(f"Jin menemukan total {sumpas} pasir, {sumbat} batu, dan {sumair} air.")
+        else : # tidak ada jin pengumpul
+            print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu.")
 
 def bangun(x) : 
     if x == 1 : # jin pembangun indiv
