580d49de755eb3ca67b33a6a946f14547a9e34c5
gav193 <116613315+gav193@users.noreply.github.com>
2023-04-19 12:38:38 +0700

    updated functions
    -nambah algoritma utk mengakses pasir,batu, dan air dari csv (tapi utk savingnya masih ragu gtw muncul sebagai file baru dengan nama yang sama atau ter-overwrite dalam file yang sama) 
    -belum menambah akses jumlah jin (n) dan saving candi baru terbangun

--- JinKumpul.py
+++ JinKumpul.py
@@ -1,19 +1,32 @@
 import csv
 import RNG as rand
-bahan = open("bahan_bangunan.csv", 'w', newline=' ')
+bahan = open("bahan_bangunan.csv", 'w')
 writer = csv.writer(bahan, delimiter=',')
+bahanR = open("bahan_bangunan.csv", 'r')
+reader = csv.reader(bahanR, delimiter=',')
 cols = ['nama', 'deskripsi', 'jumlah']
 def kumpul(x) : 
     if x == "indiv" : # jin pengumpul indiv
         pasir = rand()
         batu = rand()
         air = rand()
+        row = next(reader)
+        tempMat = [0,0,0]
+        for row in csv.reader(bahanR) :
+            tempMat = int(row[2])
+        pasir_dari_csv = tempMat[0]
+        batu_dari_csv = tempMat[1]
+        air_dari_csv = tempMat[2]
         print(f"Jin menemukan {pasir} pasir, {batu} batu, dan {air} air.")
         # isi file dalam csv bahan
-        #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
-        #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
-        #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
+        writer.writerow(['pasir', 'p', str(pasir + pasir_dari_csv)])
+        writer.writerow(['batu', 'b', str(batu + batu_dari_csv)])
+        writer.writerow(['air', 'a', str(air + air_dari_csv)])
     else : # batch kumpul
+        row = next(reader)
+        pasir_dari_csv = (row[0], int(row[2])
+        batu_dari_csv = (row[1], int(row[2])
+        air_dari_csv = (row[2], int(row[2])
         n =  # jumlah jin pengumpul
         if n > 0 : # ada jin pengumpul
             pasir = [0 for i in range(n)]
@@ -32,10 +45,15 @@ def kumpul(x) :
                 sumair += mat[2][j]
             print(f"Mengerahkan {n} jin untuk mengumpulkan bahan.")
             print(f"Jin menemukan total {sumpas} pasir, {sumbat} batu, dan {sumair} air.")
+            for row in csv.reader(bahanR) :
+                tempMat = int(row[2])
+            pasir_dari_csv = tempMat[0]
+            batu_dari_csv = tempMat[1]
+            air_dari_csv = tempMat[2]
             # isi dalam file csv bahan
-            #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
-            #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
-            #writer.writerow({'nama': '', 'deskripsi': '', 'jumlah' : ''})
+            writer.writerow(['pasir', 'p', str(sumpas + pasir_dari_csv)])
+            writer.writerow(['batu', 'b', str(sumbat + batu_dari_csv)])
+            writer.writerow(['air', 'a', str(sumair + air_dari_csv)])
         else : # tidak ada jin pengumpul
             print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu.")
 
@@ -43,24 +61,32 @@ def bangun() :
     n = # jumlah jin pembangun
     if n > 0 : # jika ada jin pembangun
         pasir = [0 for i in range(n)]
-                batu = [0 for i in range(n)]
-                air = [0 for i in range(n)]
-                mat = [pasir,batu,air]
-                for i in range(3) : 
-                    for j in range(n) :
-                        mat[i][j] = rand()
-                sumpas = 0
-                sumbat = 0 
-                sumair = 0
-                for i in range(n) :
-                    sumpas += mat[0][j]
-                    sumbat += mat[1][j]
-                    sumair += mat[2][j]
+        batu = [0 for i in range(n)]
+        air = [0 for i in range(n)]
+        mat = [pasir,batu,air]
+        for i in range(3) : 
+            for j in range(n) :
+                mat[i][j] = rand()
+        sumpas = 0
+        sumbat = 0 
+        sumair = 0
+        for i in range(n) :
+            sumpas += mat[0][j]
+            sumbat += mat[1][j]
+            sumair += mat[2][j]
+        for row in csv.reader(bahanR) :
+            tempMat = int(row[2])
+        pasir_dari_csv = tempMat[0]
+        batu_dari_csv = tempMat[1]
+        air_dari_csv = tempMat[2]
         # check kesesuaian jumlah material dari csv file 
         if sumpas <= pasir_dari_csv and sumbat <= batu_dari_csv and sumair <= air_dari_csv :
             print(f"Mengerahkan {n} jin untuk membangun candi dengan total bahan {sumpas} pasir, {sumbat} batu, dan {sumair} air")
             print(f"Jin berhasil membangun total {n} candi")
             # kurangi material di csv bahan
+            writer.writerow(['pasir', 'p', str(pasir_dari_csv - sumpas)])
+            writer.writerow(['batu', 'b', str(batu_dari_csv - sumbat)])
+            writer.writerow(['air', 'a', str(air_dari_csv - sumair)])
             # save candi yang udah dibangun di csv candi
         else : # jumlah material di csv tidak mencukupi
             print("Bangun gagal. Kurang ", end="")
