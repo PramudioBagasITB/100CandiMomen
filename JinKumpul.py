4f11acf4180d9df2d1eb91bfc595c33f87865222
gav193 <116613315+gav193@users.noreply.github.com>
2023-04-22 15:23:32 +0700

    update (w/ ##upd)

--- JinKumpul.py
+++ JinKumpul.py
@@ -19,9 +19,9 @@ def kumpul(x) : # for kumpul biasa dan batch kumpul
     tempMat = [0,0,0] # utk nyimpen data pasir,batu,air sementara 
     for i in range(3) : 
         tempMat[i] = bahan[i][2] ##upd
-    pasir_dari_csv = tempMat[0]
-    batu_dari_csv = tempMat[1]
-    air_dari_csv = tempMat[2]
+    pasir_dari_csv = int(tempMat[0])
+    batu_dari_csv = int(tempMat[1])
+    air_dari_csv = int(tempMat[2])
     
     if x == "indiv" : # jin pengumpul indiv (kumpul biasa)
         pasir = rand() # note : function rand belum dikasih parameter
@@ -58,18 +58,25 @@ def kumpul(x) : # for kumpul biasa dan batch kumpul
 
 def bangun() : 
     totJinban = 0
-    rowU3 = next(readerU)
-    while not EOP(rowU3) : 
-        if rowU3[2] == "jin_pembangun" :
-            totJinban += 1
-    n = totJinban# jumlah jin pembangun
+    for i in range(102) : ##upd
+        if user[2][i] == "jin_pembangun" :
+            totJinban += 1 
+    # list nama jin
+    names = ["*" for i in range(totJinban)]
+    for i in range(102) : 
+        idx = 0
+        if user[2][i] == "jin_pembangun" :
+            names[idx] = user[0][i]
+            idx += 1
+    
+    n = totJinban # jumlah jin pembangun
     if n > 0 : # jika ada jin pembangun
         pasir = [0 for i in range(n)]
         batu = [0 for i in range(n)]
         air = [0 for i in range(n)]
         mat = [pasir,batu,air]
         for i in range(3) : 
-            for j in range(n) :
+            for j in range(n) : # matrix utk data pasir, batu, air yang dibutuhkan
                 mat[i][j] = rand()
         sumpas = 0
         sumbat = 0 
@@ -78,20 +85,23 @@ def bangun() :
             sumpas += mat[0][j]
             sumbat += mat[1][j]
             sumair += mat[2][j]
-        for row in csv.reader(bahanR) :
-            tempMat = int(row[2])
-        pasir_dari_csv = tempMat[0]
-        batu_dari_csv = tempMat[1]
-        air_dari_csv = tempMat[2]
+        tempMat = [0,0,0] # utk nyimpen data pasir,batu,air sementara 
+        for i in range(3) : 
+            tempMat[i] = bahan[i][2] ##upd
+        pasir_dari_csv = int(tempMat[0])
+        batu_dari_csv = int(tempMat[1])
+        air_dari_csv = int(tempMat[2])
         # check kesesuaian jumlah material dari csv file 
         if sumpas <= pasir_dari_csv and sumbat <= batu_dari_csv and sumair <= air_dari_csv :
             print(f"Mengerahkan {n} jin untuk membangun candi dengan total bahan {sumpas} pasir, {sumbat} batu, dan {sumair} air")
             print(f"Jin berhasil membangun total {n} candi")
             # kurangi material di csv bahan
-            writer.writerow(['pasir', 'p', str(pasir_dari_csv - sumpas)])
-            writer.writerow(['batu', 'b', str(batu_dari_csv - sumbat)])
-            writer.writerow(['air', 'a', str(air_dari_csv - sumair)])
-            # save candi yang udah dibangun di csv candi
+            totMat = [["pasir", "p", str(pasir_dari_csv-sumpas)], ["batu", "b",str(batu_dari_csv-sumbat)], ["air", "a", str(air_dari_csv-sumair)]]
+            write_bahan(bahan_bangunan, totMat) ##upd
+            for i in range(n) : 
+                matcan = [str(i+1), names[i], mat[0][i], mat[1][i], mat[2][i]] # id ;nama ;pasir ;batu ;air
+            write_candi("candi.csv", matcan) ##upd
+            # write candi + bahan yg digunakan yang udah dibangun di csv candi
         else : # jumlah material di csv tidak mencukupi
             print("Bangun gagal. Kurang ", end="")
             if sumpas > pasir_dari_csv and sumbat <= batu_dari_csv and sumair <= air_dari_csv : # cuma pasir yang kurang
